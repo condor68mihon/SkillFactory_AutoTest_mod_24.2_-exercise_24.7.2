5350eabe09e02355d30aaaf81dad2e2dccd421d2
Mikhail <132085643+condor68mihon@users.noreply.github.com>
2023-08-03 18:33:29 +0300

    Add file settings.py
    Load valid_email and valid_password

---
+++ settings.py
@@ -0,0 +1,14 @@
+# import os
+#
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+# valid_email = os.getenv('valid_email')
+# valid_password = os.getenv('valid_password')
+
+# valid_email = "vasya@mail.com"
+# valid_password = "12345"
+
+valid_email = "mmmm@mail.ru"
+valid_password =  "mmmm"
